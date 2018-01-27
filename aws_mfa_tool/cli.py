5e496eb56af348e38a7490297ea2479474f8eadc
Phil Hachey <hachey.philippe@gmail.com>
2018-01-27 00:59:46 -0400

    consolidated commands to get-token

--- aws_mfa_tool/cli.py
+++ aws_mfa_tool/cli.py
@@ -1,10 +1,12 @@
 import click
 import boto3
 import os
-import random
+import time
 import string
+import ConfigParser
 
 from getpass import getpass
+from os.path import expanduser
 
 
 @click.group()
@@ -13,79 +15,8 @@ def cli():
 
 
 @cli.command(
-    'create',
+    'get-token',
     help='Gets STS session token')
-@click.option(
-    '-r', '--region',
-    help='AWS region')
-@click.option(
-    '-p', '--profile',
-    help='AWS shared credentials profile')
-@click.option(
-    '-m', '--mfa-serial',
-    help='MFA serial ARN')
-@click.option(
-    '-t', '--token-code',
-    help='MFA token code. If this is ommitted and --mfa-serial is given, '
-         'you will be prompted for a code')
-@click.option(
-    '-d', '--duration', default=86400,
-    help='STS token TTL in seconds')
-@click.option(
-    '-o', '--save-output-profile',
-    help='Shared credentials profile name to be written / overwritten')
-@click.option(
-    '-s', '--skip-save', is_flag=True,
-    help='Skip save to shared credentials')
-def create(
-    region,
-    profile,
-    mfa_serial,
-    duration,
-    token_code,
-    skip_save,
-    save_output_profile
-):
-    session = boto3.Session(
-        profile_name=profile,
-        region_name=region)
-    client = session.client('sts')
-
-    params = {}
-
-    if mfa_serial is not None:
-        params['SerialNumber'] = mfa_serial
-
-    if duration is not None:
-        params['DurationSeconds'] = duration
-
-    if token_code is not None:
-        params['TokenCode'] = token_code
-    elif mfa_serial is not None:
-        params['TokenCode'] = getpass('Input MFA code: ')
-
-    response = client.get_session_token(**params)
-
-    if not skip_save:
-        if save_output_profile is None:
-            save_output_profile = '_{}'.format(profile)
-
-        aws_secret_access_key = response['Credentials']['SecretAccessKey']
-        aws_access_key_id = response['Credentials']['AccessKeyId']
-        aws_session_token = response['Credentials']['SessionToken']
-
-        write_profile(save_output_profile,  {
-            'aws_secret_access_key': aws_secret_access_key,
-            'aws_access_key_id': aws_access_key_id,
-            'aws_session_token': aws_session_token,
-        })
-
-    print response
-
-
-@cli.command(
-    'assume-role',
-    help='STS assume role')
 @click.option(
     '-r', '--region',
     help='AWS region')
@@ -94,83 +25,93 @@ def create(
     default='default',
     help='AWS shared credentials profile')
 @click.option(
-    '-r', '--role-arn',
-    required=True,
-    help='AWS shared credentials profile')
-@click.option(
-    '--role-session-name',
-    help='AWS shared credentials profile')
-@click.option(
-    '-m', '--mfa-serial',
-    help='MFA serial ARN')
-@click.option(
-    '-t', '--token-code',
-    help='MFA token code. If this is ommitted and --mfa-serial is given, '
-         'you will be prompted for a code')
-@click.option(
-    '-d', '--duration',
-    default=3600,
+    '-d', '--duration', default=3600,
     help='STS token TTL in seconds')
 @click.option(
-    '-o', '--save-output-profile',
+    '-o', '--output-profile',
     help='Shared credentials profile name to be written / overwritten')
-@click.option(
-    '-s', '--skip-save',
-    is_flag=True,
-    help='Skip save to shared credentials')
-def create(
+def get_token(
     region,
     profile,
-    role_arn,
-    role_session_name,
-    mfa_serial,
     duration,
-    token_code,
-    skip_save,
-    save_output_profile
+    output_profile
 ):
-    session = boto3.Session(
-        profile_name=profile,
-        region_name=region)
-    client = session.client('sts')
+    profile_name = profile
+    all_profiles = get_configs()
+
+    profile = all_profiles.get(profile_name)
+    if profile is None:
+        raise Exception('profile {} not found'.format(profile_name))
+
 
     params = {
-        'RoleArn': role_arn
+        'DurationSeconds': duration,
     }
+    if profile.mfa_serial is not None:
+        params['SerialNumber'] = profile.mfa_serial
+        params['TokenCode'] = getpass('MFA ({}): '.format(profile.mfa_serial))
 
-    if mfa_serial is not None:
-        params['SerialNumber'] = mfa_serial
+    if profile.is_assume_role:
+        session = boto3.Session(
+            aws_access_key_id=profile.source_profile.aws_access_key_id,
+            aws_secret_access_key=profile.source_profile.aws_secret_access_key,
+            region_name=region)
+        client = session.client('sts')
 
-    if duration is not None:
-        params['DurationSeconds'] = duration
+        params['RoleArn'] = profile.role_arn
+        params['RoleSessionName'] = str(time.time())
 
-    if token_code is not None:
-        params['TokenCode'] = token_code
-    elif mfa_serial is not None:
-        params['TokenCode'] = getpass('Input MFA code: ')
-
-    if role_session_name is None:
-        params['RoleSessionName'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+        response = client.assume_role(**params)
     else:
-        params['RoleSessionName'] = role_session_name
+        session = boto3.Session(
+            aws_access_key_id=profile.aws_access_key_id,
+            aws_secret_access_key=profile.aws_secret_access_key,
+            region_name=region)
+        client = session.client('sts')
+        response = client.get_session_token(**params)
 
-    response = client.assume_role(**params)
 
-    if not skip_save:
-        if save_output_profile is None:
-            save_output_profile = '_{}'.format(profile)
+    if output_profile is None:
+        output_profile = '_{}'.format(profile_name)
 
-        aws_secret_access_key = response['Credentials']['SecretAccessKey']
-        aws_access_key_id = response['Credentials']['AccessKeyId']
-        aws_session_token = response['Credentials']['SessionToken']
+    aws_secret_access_key = response['Credentials']['SecretAccessKey']
+    aws_access_key_id = response['Credentials']['AccessKeyId']
+    aws_session_token = response['Credentials']['SessionToken']
 
-        write_profile(save_output_profile,  {
-            'aws_secret_access_key': aws_secret_access_key,
-            'aws_access_key_id': aws_access_key_id,
-            'aws_session_token': aws_session_token,
-        })
+    write_profile(output_profile,  {
+        'aws_secret_access_key': aws_secret_access_key,
+        'aws_access_key_id': aws_access_key_id,
+        'aws_session_token': aws_session_token,
+    })
 
     print response
+
+
+def get_configs():
+    configs = {}
+
+    credential_parser = ConfigParser.ConfigParser()
+    credential_parser.read(expanduser('~/.aws/credentials'))
+
+    for section in credential_parser.sections():
+        data = dict(credential_parser.items(section))
+        configs[section] = Config(section, **data)
+
+    config_parser = ConfigParser.ConfigParser()
+    config_parser.read(expanduser('~/.aws/config'))
+
+    for section in config_parser.sections():
+        profile_name = section.split(' ')[1]
+        data = dict(config_parser.items(section))
+        if profile_name in configs:
+            configs[profile_name].data.update(data)
+        else:
+            configs[profile_name] = Config(profile_name, **data)
+
+        if configs[profile_name].source_profile:
+            configs[profile_name].source_profile = configs[configs[profile_name].source_profile]
+
+    return configs
 
 
 def write_profile(profile, values):
@@ -184,6 +125,19 @@ def write_profile(profile, values):
         ))
 
     print 'Saved credentials profile "{}"!'.format(profile)
+
+
+class Config(object):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.data = kwargs
+
+    @property
+    def is_assume_role(self):
+        return self.role_arn
+
+    def __getattr__(self, name):
+        return self.data.get(name)
 
 
 if __name__ == '__main__':
